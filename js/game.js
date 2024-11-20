cb37526399c59507d6691de9fb41a88661459958
Mike Byrd <122894342+iByrd@users.noreply.github.com>
2024-11-19 19:16:10 -0500

    base background setup

--- js/game.js
+++ js/game.js
@@ -4,7 +4,13 @@ const game = new Phaser.Game(480, 320, Phaser.CANVAS, null, {
     create,
     update,
   });
+
+  function preload() {
+    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    game.scale.pageAlignHorizontally = true;
+    game.scale.pageAlignVertically = true;
+    game.stage.backgroundColor = "#eee";
+  }
   
-  function preload() {}
   function create() {}
   function update() {}
