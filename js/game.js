9e56cb516c50aa866b3987797fe4da0f2a1df106
Mike Byrd <122894342+iByrd@users.noreply.github.com>
2024-11-19 19:09:48 -0500

    initial commit

---
+++ js/game.js
@@ -0,0 +1,10 @@
+
+const game = new Phaser.Game(480, 320, Phaser.CANVAS, null, {
+    preload,
+    create,
+    update,
+  });
+  
+  function preload() {}
+  function create() {}
+  function update() {}
