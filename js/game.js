a06630c2716d34ac646ae9c6ad30d02bc10ef52d
pantchzk <pantchzk@mail.uc.edu>
2024-11-26 19:49:32 -0500

    Added loading and sprite variable declarations for paddle and brick images for later use.

--- js/game.js
+++ js/game.js
@@ -5,17 +5,28 @@ const game = new Phaser.Game(480, 320, Phaser.CANVAS, null, {
     update,
   });
 
+let ball;
+
   function preload() {
+    game.stage.smoothed = false;
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     game.scale.pageAlignHorizontally = true;
     game.scale.pageAlignVertically = true;
     game.stage.backgroundColor = "#eee";
+  
+    game.load.image("ball", "/assets/ball.png")
+    
+    //Uncomment when testing paddle and brick
+    //game.load.image("paddle", "/assets/paddle.png")
+    //game.load.image("brick", "/assets/brick.png")
   }
   
   function create() {
     var ball = game.add.sprite(50, 50, "ball");
-    var paddle = game.add.sprite(75, 10, "paddle");
-    var brick = game.add.sprite(50, 20, "brick");
+    
+    //Uncomment these after loading and implementing paddle and bricks
+    //var paddle = game.add.sprite(75, 10, "paddle");
+    //var brick = game.add.sprite(50, 20, "brick");
   }
 
   function update() {}
