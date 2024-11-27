aa6061a97d9e323e50eda799443ad5a7c59aeb4c
pantchzk <pantchzk@mail.uc.edu>
2024-11-26 19:25:47 -0500

    Added variables for ball, paddle, and brick sprites

--- js/game.js
+++ js/game.js
@@ -12,5 +12,10 @@ const game = new Phaser.Game(480, 320, Phaser.CANVAS, null, {
     game.stage.backgroundColor = "#eee";
   }
   
-  function create() {}
+  function create() {
+    var ball = game.add.sprite(50, 50, "ball");
+    var paddle = game.add.sprite(75, 10, "paddle");
+    var brick = game.add.sprite(50, 20, "brick");
+  }
+
   function update() {}
