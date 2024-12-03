2db94bd2ea565e3ea5b669846587235bae75b240
derossca <derossca@mail.uc.edu>
2024-12-03 17:45:24 -0500

    made the player paddle and controls for the paddle

--- js/game.js
+++ js/game.js
@@ -6,6 +6,7 @@ const game = new Phaser.Game(480, 320, Phaser.CANVAS, null, {
   });
 
 let ball;
+let paddle;
 
   function preload() {
     game.stage.smoothed = false;
@@ -15,23 +16,30 @@ let ball;
     game.stage.backgroundColor = "#eee";
   
     game.load.image("ball", "/assets/ball.png")
+    game.load.image("paddle", "/assets/paddle.png")
     
     //Uncomment when testing paddle and brick
-    //game.load.image("paddle", "/assets/paddle.png")
     //game.load.image("brick", "/assets/brick.png")
   }
   
   function create() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
-    ball = game.add.sprite(50, 50, "ball");
+    ball = game.add.sprite(game.world.width * 0.5, game.world.height - 25, "ball");
+    ball.anchor.set(0.5);
     game.physics.enable(ball, Phaser.Physics.ARCADE);
-    ball.body.velocity.set(150, 150);
+    ball.body.velocity.set(150, -150);
     ball.body.collideWorldBounds = true;
     ball.body.bounce.set(1);
     //Uncomment these after loading and implementing paddle and bricks
     //var paddle = game.add.sprite(75, 10, "paddle");
+    paddle = game.add.sprite(game.world.width *0.5, game.world.height - 5, "paddle");
+    paddle.anchor.set(0.5, 1);
     //var brick = game.add.sprite(50, 20, "brick");
+
+    game.physics.enable(paddle, Phaser.Physics.ARCADE);
   }
 
   function update() {
+    game.physics.arcade.collide(ball, paddle);
+    paddle.x = game.input.x || game.world.width * 0.5;
   }
